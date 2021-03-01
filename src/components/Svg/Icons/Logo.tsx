929ec1186bc5c182297573eb4ecd1244696c667b
mineyeu <andreimineyeu@gmail.com>
2021-03-01 16:10:09 +0200

    updated logo

--- src/components/Svg/Icons/Logo.tsx
+++ src/components/Svg/Icons/Logo.tsx
@@ -4,9 +4,7 @@ import { SvgProps } from "../types";
 
 const Icon: React.FC<SvgProps> = (props) => {
   return (
-    <Svg viewBox="0 0 32 32" {...props}>
-      <image width="32" height="32" href="images/egg/logo.png"/>
-    </Svg>
+    <image width="32" height="32" href="images/couscous/logo.png"/>
   );
 };
 
